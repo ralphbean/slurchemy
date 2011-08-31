6146ffb1743d7e63a1184803dfb5eb5c0335bad6
Ralph Bean <ralph.bean@gmail.com>
2011-08-31 13:58:14 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1.1'
+version = '0.1.2'
 
 with open(os.sep.join(__file__.split(os.sep)[:-1] + ["README.rst"])) as f:
     long_description = f.read().split(".. split here")[1]
